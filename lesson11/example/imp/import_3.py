bc354958aaadb731852181c198465ab96d815dab
bokus <bokus@tut.by>
2026-03-18 22:44:14 +0300

    lesson11_1

--- lesson11/example/imp/import_3.py
+++ lesson11/example/imp/import_3.py
@@ -1,3 +1,9 @@
+
+# так можно добавить путь к поиску - в данном случае родительская папка
+# import sys
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) 
+
+
 # from ..import_2 import f # папка выше
 from import_2 import f2
 from .import_4 import f5
